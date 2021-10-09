924418d022aa2c76e517af6e4c56e49037b5bd3e
Hasan Küçük <hasan.16.kucuk@gmail.com>
2021-10-09 17:13:08 +0300

    adds implementations

---
+++ common/src/main/java/com/techcareer/mobileapphackathon/common/BaseFragment.kt
@@ -0,0 +1,9 @@
+package com.techcareer.mobileapphackathon.common
+
+import androidx.databinding.ViewDataBinding
+
+/**
+ * @author: Hasan Küçük on 9.10.2021
+ */
+abstract class BaseFragment<VB : ViewDataBinding> {
+}
